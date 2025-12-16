edc350a715020f9042593ae18478cd5c8bfccb6c
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-12-16 18:35:43 +0900

    mysql setting

---
+++ 04-nest-typeorm/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: 3306,
+      username: 'root',
+      password: process.env.MYSQL_PASSWORD,
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
